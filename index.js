f18833e8fb0f87ec32abefa2d4a90f220c6eaf9f
AlexandreMagnesium <49409332+alexandremg999@users.noreply.github.com>
2020-12-05 00:57:46 +0200

    start

---
+++ index.js
@@ -0,0 +1,45 @@
+// Load env variables
+require('dotenv').config({ path: `${__dirname}/.env` })
+
+const Telegraf = require('telegraf')
+const bot = new Telegraf(process.env.TOKEN)
+
+const cron = require('node-cron')
+
+const day = ["Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"]
+const month = ["січня", "лютого", "беререзня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня"]
+
+
+bot.start( ctx => ctx.reply(
+`Привіт, ${ctx.from.first_name}!
+Я – бот розроблений @syn_mamynoi_podruhy, щоб зручно вести щоденик.
+Підключи мене до свого щоденника, як це вказано в інструкції нижче.`))
+	
+
+bot.command("work", (ctx) => {
+	ctx.reply("YES!!! -3!!! uuhhhuuuuu!!!!")
+})
+
+
+cron.schedule('0 7 * * *', () => {
+	bot.telegram.sendMessage(process.env.CHANNEL,
+`<b>${day[new Date().getDay()]}, ${new Date().getDate()} ${month[new Date().getMonth()]} ${new Date().getFullYear()}</b>
+
+
+
+<b>П'ять речей, зроблених за день, якими я пишаюсь:</b>
+
+<b>1. </b>
+<b>2. </b>
+<b>3. </b>
+<b>4. </b> 
+<b>5. </b>`, {parse_mode : "HTML"})
+})
+
+
+bot.launch({
+	webhook: {
+		domain: process.env.LOCALTUNNEL,
+		port: 3000
+	}
+})
