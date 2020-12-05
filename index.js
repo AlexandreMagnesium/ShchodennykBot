f2cca9b7c133e262eecb356afc0ce7adfaada47b
Alexandre <49409332+AlexandreMagnesium@users.noreply.github.com>
2020-12-05 23:46:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 require('dotenv').config()
 
 const Telegraf = require('telegraf')
-const bot = new Telegraf(process.env.TOKEN)
+const bot = new Telegraf(TOKEN)
+//const bot = new Telegraf(process.env.TOKEN)
 
 const cron = require('node-cron')
 
